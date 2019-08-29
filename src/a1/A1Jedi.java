0a5d0525740b233e4b10714dedf7008dafeffae8
Sung <sungsong@live.unc.edu>
2019-08-28 23:33:46 -0400

    jedi

--- src/a1/A1Jedi.java
+++ src/a1/A1Jedi.java
@@ -5,10 +5,52 @@ import java.util.Scanner;
 public class A1Jedi {
 
 	public static void main(String[] args) {
-		
-		Scanner scan = new Scanner(System.in);
 
-		// Your code follows here.
+		Scanner scan = new Scanner(System.in);
+ 
+		int numOfItems = scan.nextInt();
+
+		String[] namesOfItems = new String[numOfItems];
+		double[] pricesOfItems = new double[numOfItems];
+ 
+		for (int i = 0; i < numOfItems; i++) {
+			namesOfItems[i] = scan.next();
+			pricesOfItems[i] = scan.nextDouble();
+		}
+
+		int numOfCustomers = scan.nextInt();
+		double[] priceTotal = new double[numOfCustomers];
+		String[] customerNames = new String[numOfCustomers];
+		int[] howManyBought = new int[numOfItems];
+		int[] totalHowManyBought = new int[numOfItems];
 		
+		// organizing customer data
+		for (int i = 0; i < numOfCustomers; i++) {
+			customerNames[i] = scan.next() + " " + scan.next();
+			int numOfItemsBought = scan.nextInt();
+			
+
+			for (int j = 0; j < numOfItemsBought; j++) {
+				int quantityEach = scan.nextInt();
+				String nameOfEach = scan.next();
+
+				for (int k = 0; k < numOfItems; k++) {
+
+					if (nameOfEach.equals(namesOfItems[k])) {
+						howManyBought[k] = howManyBought[k] + quantityEach;
+					}
+					
+					// checking specific item arr
+//					System.out.println(i + ". " + j + ". " + howManyBought[k]);
+				}
+				// just to make output look readable when testing
+//				System.out.println();
+			}
+			
+			for (int j = 0; j < numOfItems; j++) {
+				totalHowManyBought[j] = howManyBought[j];
+				System.out.println(totalHowManyBought[j] + " " + namesOfItems[j]);
+			}			
+		}
 	}
 }
